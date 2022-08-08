08bafbf6efa87a2203b925da4a1273b859e1eccb
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-08 00:36:19 -0400

    clean comment of toast

--- config/toastConfig.tsx
+++ config/toastConfig.tsx
@@ -1,26 +1,9 @@
-//https://github.com/calintamas/react-native-toast-message/blob/05a86209417762183dda8ddcb4c4d4f2d3839e3f/docs/custom-layouts.md
-import {View,Text} from "react-native";
+
 import {ToastConfig} from "react-native-toast-message/lib/src/types";
 import {BaseToast} from "react-native-toast-message";
 
+//https://github.com/calintamas/react-native-toast-message/blob/05a86209417762183dda8ddcb4c4d4f2d3839e3f/docs/custom-layouts.md
 const toastConfig:ToastConfig = {
-    /*
-      Overwrite 'success' type,
-      by modifying the existing `BaseToast` component
-    */
-
-    /*
-      Overwrite 'error' type,
-      by modifying the existing `ErrorToast` component
-    */
-
-    /*
-      Or create a completely new type - `tomatoToast`,
-      building the layout from scratch.
-
-      I can consume any custom `props` I want.
-      They will be passed when calling the `show` method (see below)
-    */
     primaryColorToast: (props) => <BaseToast style={{ borderLeftColor: '#2F84F1' }} {...props} />
 
 };
