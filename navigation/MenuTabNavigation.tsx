f602e3ac6d36765e3f464b26c9d1463d741636e0
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-09 20:35:48 -0400

    attempt bottom to move to top fail

--- navigation/MenuTabNavigation.tsx
+++ navigation/MenuTabNavigation.tsx
@@ -1,28 +1,30 @@
 import React from "react";
-
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import TabNavigator from "../components/TabNavigator";
 import ProductScreen from "../screen/ProductScreen";
 import ProductCategoryScreen from "../screen/ProductCategoryScreen";
 import {useTheme} from "@rneui/themed";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
+import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
 export type MenuParamsList = {
     PRODUCTS: undefined;
     CATEGORIES: undefined;
 };
 const Tab = createMaterialTopTabNavigator<MenuParamsList>();
+// const Tab = createMaterialBottomTabNavigator<MenuParamsList>();
 
+//TODO: unable to remove animation
+//will have to do for now
 export default () => {
     const theme = useTheme();
   return (
     <Tab.Navigator initialRouteName={"PRODUCTS"}
                    tabBar={(props) => <TabNavigator {...props} />}
-        // screenOptions={{
-        //     // tabBarStyle: { backgroundColor: "red" },
-        //     // tabBarLabelStyle: {color:"white"},
-        //     tabBarActiveTintColor: theme.theme.colors.primary,
-        //     tabBarInactiveTintColor: "yellow",
-        //     // h
-        // }}
+                   // tabBarPosition="bottom"
+
+        screenOptions={{
+
+        }}
     >
         <Tab.Screen component={ProductScreen} name="PRODUCTS"
                     //options={{title:"PRODUCTS"}}
