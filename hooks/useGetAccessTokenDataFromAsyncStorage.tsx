d9a1cdc747eb39a50eea998597fc89f1ca762224
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-14 14:56:52 -0400

    app works here splash screen also

--- hooks/useGetAccessTokenDataFromAsyncStorage.tsx
+++ hooks/useGetAccessTokenDataFromAsyncStorage.tsx
@@ -2,14 +2,14 @@ import {useCallback, useEffect, useState} from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import useDispatchUserAuth from "./useDispatchUserAuth";
 import * as SplashScreen from "expo-splash-screen";
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 export default () => {
 
     const handleDispatchUserAuth = useDispatchUserAuth();
 
     useEffect(() => {
         (async () => {
-
+            await SplashScreen.preventAutoHideAsync();//this also worked by awaiting inside of async
             const access_token = await AsyncStorage.getItem("token")
             const token_type = await AsyncStorage.getItem("token_type")
             await handleDispatchUserAuth({access_token,token_type});
