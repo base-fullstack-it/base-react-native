feae3b5dd4a41abdde7bbd4822f7e7cbca8b152a
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-15 00:28:17 -0400

    font sizef rom google to work and iron out kinks of signup

--- screen/SignupScreen.tsx
+++ screen/SignupScreen.tsx
@@ -4,10 +4,19 @@ import {Card} from "@rneui/themed";
 import GrasspFormLogo from "../components/GrasspFormLogo";
 import SplashScreen from "./SplashScreen";
 import SplashView from "../components/SplashView";
+import {useEffect, useState} from "react";
 
 export default () => {
 
+    // const [loadingWithTimeout, setLoadingWithTimeout] = useState(true);
     const {handleRegister,isLoading,isRegisterSuccess,registerData} = useRegister();
+    // useEffect(() => {
+    //     if(!isLoading) {
+    //         setTimeout(() => {
+    //             setLoadingWithTimeout(false);
+    //         }, 1500);
+    //     }
+    // }, [isLoading]);
 
     return (
         isLoading?
