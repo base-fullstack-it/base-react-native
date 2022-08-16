3d2f40b2b732e99eef223f980731a71f8a236f24
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-15 20:30:55 -0400

    cleanse thy code

--- hooks/menu/useMenuFactory.tsx
+++ hooks/menu/useMenuFactory.tsx
@@ -2,11 +2,10 @@ import MenuLocationTypes from "../../model/enum/MenuLocationTypes";
 import {useLazyGetMenuByZipCodeQuery} from "../../services/menuApiSlice";
 import {useEffect} from "react";
 import {useLoggedInUserQuery} from "../../services/userApiSlice";
-import {UserDTO} from "../../model/dto/UserDTO";
 
 export default (menuType:MenuLocationTypes) => {
     const{ data } = useLoggedInUserQuery();
-    const [trigger, result] = useLazyGetMenuByZipCodeQuery();
+    const [triggerGetMenuByZipCode, result] = useLazyGetMenuByZipCodeQuery();
     useEffect(()=>{
         if(!data) return;
         switch (menuType) {
@@ -22,19 +21,9 @@ export default (menuType:MenuLocationTypes) => {
                 //     });
                 break;
             case MenuLocationTypes.DEFAULT_MENU:
-                console.log(data,'userFromDB')
-                // console.log(data!.ds,'userFromDB')
-                // console.log(data!.defaultAddress!.zipCode,'im just a zipcode')
-                // if (loggedInUser.signupZipCode) {
-                //     dispatch<any>(getMenuByZipCode(loggedInUser.signupZipCode, dsprId, isFullMenuShown))
-                //         .then(response => checkForClosedMessageAndRenderMenu(response, dsprId));
-                // } else {
+                triggerGetMenuByZipCode({zipCode:data!.defaultAddress!.zipCode});
+                // could be not needed for user app
                 //     setShowPleaseEnableLocation(true);
-                // }
-                //https://stackoverflow.com/questions/71724449/how-can-i-call-rtk-query-hook-when-the-condition-changes
-                // trigger({zipCode:data!.defaultAddress!.zipCode,dsprId: 1,isFullMenuShown:false});
-                trigger({zipCode:data!.defaultAddress!.zipCode});
-                // console.log(result,' we did it');
                 break;
             default:
                 break;
