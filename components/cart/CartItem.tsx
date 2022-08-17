38821e0253c742c1fe087bfb9b2277d496df5bd3
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-17 19:37:23 -0400

    state of cart was saved between app refresh!!

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -1,9 +1,15 @@
 import {View,Text} from "react-native";
-import {ListItem} from "@rneui/themed";
-
+import {Button, ListItem} from "@rneui/themed";
+import {useAppDispatch} from "../../app/hooks";
+import {decrease, increase, remove} from "../../feature/cart/cartSlice";
 export default ({item}:{item:any}) => {
+    const dispatch = useAppDispatch();
     return<ListItem>
-        <Text>LOL</Text>
+        {/*<Text>title:{item.title}</Text>*/}
+        <Text>price:{item.price}</Text>
+        <Button onPress={()=>dispatch(decrease(item.id))} title={"<"}/>
+        <Text>{item.amount}</Text>
+        <Button onPress={()=>dispatch(increase(item.id))} title={">"}/>
     </ListItem>
 
 }
