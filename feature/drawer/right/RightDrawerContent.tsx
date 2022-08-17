d4c6a0422607d6de3da4b83c61e2d922b3fee4e9
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-17 19:45:39 -0400

    rmv comment

--- feature/drawer/right/RightDrawerContent.tsx
+++ feature/drawer/right/RightDrawerContent.tsx
@@ -28,8 +28,6 @@ export default (props: DrawerContentComponentProps) => {
         }}
         />
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                {console.log(items.length,'itemsitems')}
-
                 <Text>View Order</Text>
                 {items.map((item:any) => <CartItem item={item}/>)}
                 <Text>${totalAmount}</Text>
