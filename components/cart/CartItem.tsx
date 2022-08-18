5e14a100010ae94817284b4e9f617429e9dbec01
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-17 20:40:38 -0400

    instead of chevron follow the sonic overlay i think

--- components/cart/CartItem.tsx
+++ components/cart/CartItem.tsx
@@ -2,14 +2,19 @@ import {View,Text} from "react-native";
 import {Button, ListItem} from "@rneui/themed";
 import {useAppDispatch} from "../../app/hooks";
 import {decrease, increase, remove} from "../../feature/cart/cartSlice";
+import {Ionicons} from "@expo/vector-icons";
 export default ({item}:{item:any}) => {
     const dispatch = useAppDispatch();
     return<ListItem>
         {/*<Text>title:{item.title}</Text>*/}
         <Text>{item.title}</Text>
+        <View style={{flexDirection:"column"}}>
+            {/*<ion-icon name="chevron-up-outline"></ion-icon>*/}
+            {/*<Ionicons name={"chevron-up-outline"}/>*/}
         <Button onPress={()=>dispatch(decrease(item.id))} title={"<"}/>
         <Text>{item.amount}</Text>
         <Button onPress={()=>dispatch(increase(item.id))} title={">"}/>
+        </View>
     </ListItem>
 
 }
