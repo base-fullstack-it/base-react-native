beb634ec31efba34f0d8f9b01cdf92f52d151b5c
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-18 08:11:16 -0400

    some styling of category with adding image as avatar creates space i need

--- screen/ProductCategoryScreen.tsx
+++ screen/ProductCategoryScreen.tsx
@@ -2,16 +2,24 @@ import {useSelector} from "react-redux";
 import {MenuState, selectMenu} from "../feature/menu/slice/menuSlice";
 import {RootState} from "../app/store";
 import {FlatList, View} from "react-native";
-import {ListItem} from "@rneui/themed";
+import {Avatar, ListItem,Image} from "@rneui/themed";
 import {ProductCategoryDTO} from "../model/dto/ProductCategoryDTO";
+import PlaceHolderImage from "../image/PlaceHolderImage";
 const renderItem = ({ item }:{item:ProductCategoryDTO}) => (
-    <ListItem bottomDivider>
+    <ListItem style={{borderWidth:0.17}} bottomDivider >
         {console.log(item," I AM FROM ZEUS")}
-        <ListItem.Content>
+        {/*<Image*/}
+        {/*    resizeMode={"contain"}*/}
+        {/*    style={{height:30}}*/}
+        {/*    source={require("../assets/splash.png")}*/}
+        {/*/>*/}
+        <ListItem.Content style={{flexDirection:"row"}}>
+
             <ListItem.Title>{item.name}</ListItem.Title>
-            <ListItem.Subtitle>{item.imageLocation}</ListItem.Subtitle>
+            {/*<ListItem.Subtitle>{item.imageLocation}</ListItem.Subtitle>*/}
         </ListItem.Content>
-        <ListItem.Chevron />
+        {/*<Avatar size={'xlarge'}source={{uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'}} />*/}
+        <Avatar size={'xlarge'} source={require("../assets/splash.png")} />
     </ListItem>
 )
 const keyExtractor = (productCategoryDTO:ProductCategoryDTO, index:number) => index.toString()
