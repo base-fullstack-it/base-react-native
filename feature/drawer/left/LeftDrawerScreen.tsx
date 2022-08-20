97b7d02940f00769babe3b44ebba1eef88e575df
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-20 19:16:37 -0400

    cleansing of thy code CART IS BROKEN

--- feature/drawer/left/LeftDrawerScreen.tsx
+++ feature/drawer/left/LeftDrawerScreen.tsx
@@ -5,9 +5,7 @@ import ViewSearchBarHeader from "../../../components/ViewSearchBarHeader";
 import BottomFABTabNavigator from "../../../navigation/bottom_tab/BottomFABTabNavigator";
 
 const LeftDrawer = createDrawerNavigator();
-//https://www.youtube.com/watch?v=aTO49WIxOfw&t=661s
 export default () => {
-    const theme = useTheme();
     return (
         <LeftDrawer.Navigator
             id="LeftDrawer"
@@ -19,15 +17,8 @@ export default () => {
             //TODO appears to be a bug where drawerContent needs a prop
             drawerContent={(props) => <LeftDrawerContent {...props} />}
         >
-            <LeftDrawer.Screen options={{
-                // header: (props) => <ViewSearchBarHeader />,
-                // headerLeft: () => <View></View>,
-                // headerTitle:() => <SearchBar/>
-                // headerStyle:{
-                //     backgroundColor: theme.theme.colors.primary
-                // }
-                // headerTitle:"grasspHealth"
-            }} name="Home" component={BottomFABTabNavigator} />
+            <LeftDrawer.Screen
+            name="Home" component={BottomFABTabNavigator} />
         </LeftDrawer.Navigator>
     );
 }
--- hooks/useLogout.tsx
+++ hooks/useLogout.tsx
@@ -2,15 +2,14 @@ import {useAppDispatch} from "../app/hooks";
 import {logout} from "../feature/auth/slice/authSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Toast from "react-native-toast-message";
-import useNavigateToLogin from "./navigation/useNavigateToLogin";
-import useNavigateToMenuTabNavigation from "./navigation/useNavigateToMenuTabNavigation";
-import {useState} from "react";
+import {clearCart} from "../feature/cart/cartSlice";
 
 type LogoutFunction = () => Promise<void>;
 
 export default ():{ handleLogout: () => Promise<void> } => {
     const dispatch = useAppDispatch();
     const handleLogout = async () => {
+        dispatch(clearCart)
         await AsyncStorage.clear();
         dispatch(logout());
         Toast.show({
