209b4eba84a59399120e69a553360a50b114fa8d
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-20 23:47:53 -0400

    added purge to logout does not break anything should technically clear out any state within redux persist? from what i understand

--- hooks/useLogout.tsx
+++ hooks/useLogout.tsx
@@ -3,14 +3,19 @@ import {logout} from "../feature/auth/slice/authSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Toast from "react-native-toast-message";
 import {clearCart} from "../feature/cart/cartSlice";
+import {persistor} from "../app/store";
+
 
 type LogoutFunction = () => Promise<void>;
 
 export default ():{ handleLogout: () => Promise<void> } => {
+
     const dispatch = useAppDispatch();
     const handleLogout = async () => {
         dispatch(clearCart())
         await AsyncStorage.clear();
+        //https://stackoverflow.com/questions/52296953/how-to-reset-value-of-state-in-a-redux-store
+        await persistor.purge();
         dispatch(logout());
         Toast.show({
             type: 'primaryGreenColorToast',
