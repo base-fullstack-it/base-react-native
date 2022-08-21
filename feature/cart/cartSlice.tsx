34de6372e535b580d504cd1e4901a96a9a8a4dbc
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-21 16:48:07 -0400

    ok nvm maybe this get cart total will work

--- feature/cart/cartSlice.tsx
+++ feature/cart/cartSlice.tsx
@@ -1,7 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 import data from "./data";
 import {ProductDTO} from "../../model/dto/ProductDTO";
 import {ProductToCartValues} from "../../model/dto/ProductToCartValues";
+
 export interface CartState {
     readonly productCartList: ReadonlyArray<ProductToCartValues>,
     readonly totalAmount: number,
@@ -14,9 +15,9 @@ const initialState: CartState = {
     totalCount: 0,
 
 };
-type numberReducer ={
-    totalAmount:number;
-    totalCount:number;
+type numberReducer = {
+    totalAmount: number;
+    totalCount: number;
 }
 const cartSlice = createSlice({
     name: "cart",
@@ -29,42 +30,51 @@ const cartSlice = createSlice({
 
     reducers: {
         getCartTotal: (state) => {
-            // let { totalAmount, totalCount } = state.productCartList && state.productCartList.reduce<numberReducer>(
-            //     (cartTotal, cartItem) => {
-            //         const {
-            //             // price,
-            //             quantity
-            //         } = cartItem;
-            //         // const itemTotal = price * quantity;
-            //         const itemTotal = 0;//TODO fix this later
-            //
-            //         cartTotal.totalAmount += itemTotal;
-            //         cartTotal.totalCount += quantity;
-            //         return cartTotal;
-            //     },
-            //     {
-            //         totalAmount : 0,
-            //         totalCount: 0,
-            //     }
-            // );
-            // state.totalAmount = parseInt(totalAmount.toFixed(2));
-            // state.totalCount = totalCount;
-            state.totalAmount= 0;
-            state.totalCount = 0;
+            if (state.productCartList && state.productCartList.length > 0) {
+                let {totalAmount, totalCount} = state.productCartList && state.productCartList.reduce<numberReducer>(
+                    (cartTotal, cartItem) => {
+                        const {
+                            // price,
+                            quantity
+                        } = cartItem;
+                        // const itemTotal = price * quantity;
+                        const itemTotal = 0;//TODO fix this later
+
+                        cartTotal.totalAmount += itemTotal;
+                        cartTotal.totalCount += quantity;
+                        return cartTotal;
+                    },
+                    {
+                        totalAmount: 0,
+                        totalCount: 0,
+                    }
+                );
+                state.totalAmount = parseInt(totalAmount.toFixed(2));
+                state.totalCount = totalCount;
+            } else {
+                state.totalAmount = 0;
+                state.totalCount = 0;
+            }
         },
         remove: (state, action) => {
             state.productCartList = state.productCartList.filter((productCartValue) => productCartValue.productDTO.id !== action.payload);
         },
         increase: (state, action) => {
             state.productCartList = state.productCartList.map((productCartValue) => {
-                if (productCartValue.productDTO.id === action.payload) return { ...productCartValue, quantity: productCartValue.quantity + 1 };
+                if (productCartValue.productDTO.id === action.payload) return {
+                    ...productCartValue,
+                    quantity: productCartValue.quantity + 1
+                };
                 else return productCartValue;
             });
         },
         decrease: (state, action) => {
             state.productCartList = state.productCartList
                 .map((productCartValue) => {
-                    if (productCartValue.productDTO.id === action.payload) return { ...productCartValue,  quantity: productCartValue.quantity - 1 };
+                    if (productCartValue.productDTO.id === action.payload) return {
+                        ...productCartValue,
+                        quantity: productCartValue.quantity - 1
+                    };
                     else return productCartValue;
                 })
                 .filter((productCartValue) => productCartValue.quantity !== 0);
@@ -73,8 +83,8 @@ const cartSlice = createSlice({
             state.productCartList = [];
         },
 
-        addProductToCart:(state,action) => {
-            console.log(action.payload,"actionpayloadYOU MADE IT HERE")
+        addProductToCart: (state, action) => {
+            console.log(action.payload, "actionpayloadYOU MADE IT HERE")
             // state.items.push(action.payload);
         }
     },
