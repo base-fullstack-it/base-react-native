017a24e87c5f9e55f8195a5081517a09ced81044
michaeljamieson <michaeljamieson10@gmail.com>
2022-08-22 00:08:51 -0400

    whole products display appropriately maybe change the way the class is made to fit the product also

--- components/product/to_cart/whole/WholeButtonFactory.tsx
+++ components/product/to_cart/whole/WholeButtonFactory.tsx
@@ -2,7 +2,6 @@ import WholeUnitButton from "./WholeUnitButton";
 import {ProductDTO} from "../../../../model/dto/ProductDTO";
 
 export default ({productDTO}:{productDTO:ProductDTO}) => {
-    console.log("DO I EVER MAAAA")
     const wholeUnitButtons = [];
     //the index is also used as a multiplier
     for(let i = 1; i<=4;i++) wholeUnitButtons.push(
--- components/product/to_cart/whole/WholeUnitButton.tsx
+++ components/product/to_cart/whole/WholeUnitButton.tsx
@@ -2,20 +2,21 @@ import {useDispatch} from "react-redux";
 import {StyleSheet} from "react-native";
 import {Button} from "@rneui/themed";
 import {ProductDTO} from "../../../../model/dto/ProductDTO";
+import {WholeProductClass} from "../../../../model/class/product/WholeProductClass";
 
 
 // export default ({idx,product, orderDetailInCartForProduct}) =>{
 export default ({idx,product}:{idx:number,product:ProductDTO}) =>{
     // const dispatch = useDispatch();
     // const handleAddProductToCart = () => dispatch(addProductToCart(product, undefined,idx));
+    const wholeProductClass = new WholeProductClass(product,idx);
 
     // const price = product.currentPrice.price * idx;
     // const displayPrice = createDisplayPrice(price);
-console.log("DO I EVER MAKE IT HERE")
     return <Button
         buttonStyle={styles.button}
         // onPress={handleAddProductToCart}
-        title={"LOLw"}
+        title={wholeProductClass.getDisplayPrice()}
         // title={displayPrice}
         // disabled={(orderDetailInCartForProduct && idx === orderDetailInCartForProduct.quantity)}
         />
