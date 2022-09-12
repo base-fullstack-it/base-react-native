d6e96b74e187b32bcddce126d208febe0d8a9601
michaeljamieson <michaeljamieson10@gmail.com>
2022-09-11 20:21:51 -0400

    rmv comment closing project for break

--- screen/SplashScreen.tsx
+++ screen/SplashScreen.tsx
@@ -2,9 +2,13 @@ import useGetAccessTokenDataFromAsyncStorage from "../hooks/useGetAccessTokenDat
 import SplashView from "../components/SplashView";
 import AuthorizationDirector from "../navigation/auth/AuthorizationDirector";
 import * as SplashScreen from "expo-splash-screen";
+import useLogout from "../hooks/useLogout";
+import {useEffect} from "react";
 
 SplashScreen.preventAutoHideAsync();
 export default () =>{
+    // const {handleLogout} = useLogout();
+    // useEffect(()=>{handleLogout},[])
 
     const isAppLoaded = useGetAccessTokenDataFromAsyncStorage();
 
